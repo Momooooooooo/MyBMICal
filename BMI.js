d832b1a85cfd3f9ae817fb455e63e0c289a039b1
Momooooooooo <50395688+Momooooooooo@users.noreply.github.com>
2020-07-14 02:27:15 +0800

    finish layout and effect
    background remaining.

--- BMI.js
+++ BMI.js
@@ -2,17 +2,23 @@
 var height = document.getElementById("height");
 var weight = document.getElementById("weight");
 var result = document.getElementById("result");
+var submit = document.getElementById("submit");
 
-height.addEventListener("input", calculation);
-weight.addEventListener("input", calculation);
+submit.addEventListener("click", calculation)
 
 function calculation() {
+
     var inputHeight = parseFloat(height.value) || 0;
     var inputWeight = parseFloat(weight.value) || 0;
 
+    if(inputHeight==0 && inputWeight==0){
+        alert("Please input correct height and weight!");
+        return;
+
+    }
     var outputResult = inputWeight / (inputHeight * inputHeight);
 
     outputResult = outputResult.toFixed(3);
-
+    
     result.innerHTML = "Your BMI is: " + outputResult;
 }
